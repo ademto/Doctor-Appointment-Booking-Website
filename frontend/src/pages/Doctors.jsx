db9f8747585d76f6f808d1c169f94c3f19337f40
ademto <emmanueladetoro72@gmail.com>
2024-09-25 23:00:46 -0400

    updates

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -9,6 +9,9 @@ const Doctors = () => {
   return (
     <div>
       <p>Browse through the Doctos specialist</p>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut itaque saepe dolores laboriosam. Assumenda omnis iste asperiores ullam velit quod dolorem quibusdam voluptate magni facilis amet nobis delectus, ad est fuga nisi alias placeat quis repudiandae quos perspiciatis. Quis praesentium molestiae distinctio! Consequatur eveniet necessitatibus quos soluta sed consectetur <accusamus className="lorem50"></accusamus>
+      </div>
     </div>
   )
 }
