c481d8aa7ec929a575bb3ac599a278e1673d6d6b
ademto <emmanueladetoro72@gmail.com>
2024-09-27 12:53:18 -0400

    updates

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -10,7 +10,14 @@ const Doctors = () => {
     <div>
       <p>Browse through the Doctos specialist</p>
       <div>
-        
+        <div>
+          <p>General Physician</p>
+          <p>Gynecologist</p>
+          <p>Dermatologist</p>
+          <p>Pediatricians</p>
+          <p>Neurologist</p>
+          <p>Gastroenterologist</p>
+        </div>
       </div>
     </div>
   )
