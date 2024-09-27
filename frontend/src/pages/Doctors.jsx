0970666efbb5bd215f232d69781fb1bb9c1f93dd
ademto <emmanueladetoro72@gmail.com>
2024-09-26 20:22:00 -0400

    updates

--- frontend/src/pages/Doctors.jsx
+++ frontend/src/pages/Doctors.jsx
@@ -10,6 +10,7 @@ const Doctors = () => {
     <div>
       <p>Browse through the Doctos specialist</p>
       <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laboriosam totam similique tempora vel veritatis ipsum exercitationem nesciunt obcaecati sunt illo, corrupti eligendi error incidunt illum commodi qui beatae inventore eius laudantium optio nihil ratione dicta fugiat enim. Iusto consectetur quidem culpa vel molestias quibusdam veritatis optio dolore quo rerum fugit ea odit, nobis ipsam at rem, ex perspiciatis sit qui voluptatibus quod hic voluptates corporis. Molestiae esse repudiandae totam vel, officia exercitationem tenetur odio consequatur perspiciatis perferendis expedita facere asperiores eos voluptate voluptas saepe! Aut beatae dolor tempora eveniet corrupti quos reiciendis in culpa error, reprehenderit nesciunt quae consequatur excepturi?
       </div>
     </div>
   )
